0857e2703b7659fce8ac36e195274fe32cad1855
prajnastra <coder.abhijit90@gmail.com>
2023-02-06 15:25:52 +0530

    add: kbar lib; remove: kdam progrss bar

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,26 @@
 use std::env;
-use kdam::{tqdm, BarExt};
 use std::str::FromStr;
-
-fn hms_to_seconds(s: &str) -> f64 {
-    let parts = s.split(":").collect::<Vec<&str>>();
-    let hours = f64::from_str(parts[0]).unwrap();
-    let minutes = f64::from_str(parts[1]).unwrap();
-    let seconds = f64::from_str(parts[2]).unwrap();
-
-    (hours * 3600.0) + (minutes * 60.0) + seconds
-}
+use kbar::{KBar, BarType};
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = parse_config(&args);
 
     let sleep_time = hms_to_seconds(config.query.as_str()) / 100.0;
+    let mut kbar = KBar::new(BarType::Bar, true, true, 20);
+    
+    kbar.clear_term().expect("Not able to clear buffer");
 
-    println!("Timer starting for {}", config.query);
+    for x in 0..100 {
+        let percentage_decimal = x as f32 / 100.0;
+        let percent = (percentage_decimal * 100.0) as u8;
 
-    let render_length = 100;
+        kbar.update(percent);
+        kbar.draw();
 
-    let mut pb = tqdm!(
-        total = render_length,
-        desc = "Timer   ",
-        animation = "fillup",
-        position = 0,
-        force_refresh = true
-    );
-
-    pb.set_postfix("");
-
-    for _ in 0..render_length {
-        pb.update(1);
-        std::thread::sleep(std::time::Duration::from_secs_f64(sleep_time));
+        sleep(Duration::from_secs_f64(sleep_time));
     }
 }
 
@@ -47,3 +34,11 @@ fn parse_config(args: &[String]) -> Config {
     Config { query }
 }
 
+fn hms_to_seconds(s: &str) -> f64 {
+    let parts = s.split(":").collect::<Vec<&str>>();
+    let hours = f64::from_str(parts[0]).unwrap();
+    let minutes = f64::from_str(parts[1]).unwrap();
+    let seconds = f64::from_str(parts[2]).unwrap();
+
+    (hours * 3600.0) + (minutes * 60.0) + seconds
+}
