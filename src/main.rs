af10fdcb7c9fb67ce3af46a2d6c021a515636a5c
prajnastra <coder.abhijit90@gmail.com>
2023-02-08 17:56:08 +0530

    add: alert sound when timer finish

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use kbar::Bar;
+use rodio::{source::Source, Decoder, OutputStream};
 use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::str::FromStr;
 use std::thread::sleep;
 use std::time::Duration;
-use std::str::FromStr;
-use kbar::Bar;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,15 +16,28 @@ fn main() {
     let mut bar = Bar::new();
     bar.set_job_label(format!("Timer {} :", config.query).as_str());
 
-
     for x in 1..101 {
         sleep(Duration::from_secs_f64(sleep_time));
         bar.reach_percent(x);
     }
+
+    // Get a output stream handle to the default physical sound device
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    // Load a sound from a file, using a path relative to Cargo.toml
+    let file = BufReader::new(File::open("assets/alert.mp3").unwrap());
+    // Decode that sound file into a source
+    let source = Decoder::new(file).unwrap();
+    // Play the sound directly on the device
+    stream_handle
+        .play_raw(source.convert_samples())
+        .expect("Not able to play sound.");
+
+    // The sound plays in a separate audio thread,
+    sleep(Duration::from_secs(2));
 }
 
 struct Config {
-    query: String
+    query: String,
 }
 
 fn parse_config(args: &[String]) -> Config {
